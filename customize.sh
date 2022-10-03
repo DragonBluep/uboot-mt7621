43050e47e4cc02b4ff3e9efef39f07180d72775a
Shiji Yang <yangshiji66@qq.com>
2022-10-03 14:06:52 +0800

    CI: enable build customized mt7621 uboot online with GitHub Actions
    Usage:
    1. Fork this repository.
    2. Go to GiHub Actions and chose Build customized u-boot.
    3. Click 'Run workflow' then configure parameters.
    4. Download binaries from Artifacts. Flash 'u-boot-mt7621.bin'.
    
    Notice:
    1. MTD partition table must match to stock firmware's partitions.
    2. Kernel load address is the hexadecimal sum of the all
    partitions size before 'firmware'.
    for example:
    partition table: 192k(u-boot),64k(u-boot-env),64k(factory),-(firmware)
    192kB + 64kB + 64kB = 320kB = 320 * 1024 B = 327680 B = 0x50000 B
    so kernel load address will be: 0x50000
    3. 'firmware' partition is essential.
    
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

---
+++ customize.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# toolchain path
+Toolchain=$(cd ../openwrt*/staging_dir/toolchain-mipsel*/bin; pwd)'/mipsel-openwrt-linux-'
+Staging=${Toolchain%/toolchain-*}
+
+echo "CROSS_COMPILE=${Toolchain}"
+echo "STAGING_DIR=${Toolchain%/toolchain-*}"
+cd $(dirname "$0")
+
+# arguments:
+# $1	string: flash type
+# $2	string: partition table
+# $3	string: kernel offset
+# $4	number: reset pin
+# $5	number: sysled gpio
+# $6	number: cpu frequency
+# $7	number: ram frequency
+# $8	string: ddr2 param
+# $9	string: ddr3 param
+
+echo "Parse flash type: $1"
+# simple check if partition table is valid
+if [ -z $( echo -n "$2" | grep '),-(firmware)') ]; then
+	echo "Invalid mtd partition table!"
+	exit 1
+fi
+DEFCONFIG="configs/mt7621_build_defconfig"
+if [ "$1" = 'SPI' ]; then
+	cp configs/mt7621_nor_template_defconfig ${DEFCONFIG}
+	echo -e "CONFIG_MTDPARTS_DEFAULT=\"mtdparts=raspi:$2\"" >> ${DEFCONFIG}
+elif [ "$1" = 'NAND' ]; then
+	cp configs/mt7621_nand_template_defconfig ${DEFCONFIG}
+	echo -e "CONFIG_MTDPARTS_DEFAULT=\"mtdparts=nand0:$2\"" >> ${DEFCONFIG}
+else
+	cp configs/mt7621_nmbm_template_defconfig ${DEFCONFIG}
+	echo -e "CONFIG_MTDPARTS_DEFAULT=\"mtdparts=nmbm0:$2\"" >> ${DEFCONFIG}
+fi
+echo "set partition table: $2"
+
+echo "set kernel offset: $3"
+if [ "$1" = 'SPI' ]; then
+	echo "CONFIG_DEFAULT_NOR_KERNEL_OFFSET=$3" >> ${DEFCONFIG}
+else
+	echo "CONFIG_DEFAULT_NAND_KERNEL_OFFSET=$3" >> ${DEFCONFIG}
+fi
+
+if [ $4 -ge 0 -a $4 -le 48 ]; then
+	echo "set reset button pin: $4"
+	echo "CONFIG_MT7621_RST_BTN_PIN=$4" >> ${DEFCONFIG}
+	# if reset button is triggered by high level, change '0' to '1' here
+	echo "CONFIG_MT7621_RST_BTN_LEVEL=0" >> ${DEFCONFIG}
+else
+	echo "Reset button is disabled!"
+	echo "# CONFIG_MT7621_RST_BTN is not set" >> ${DEFCONFIG}
+fi
+
+if [ $5 -ge 0 -a $5 -le 48 ]; then
+	echo "set system led pin: $5"
+	echo "CONFIG_MT7621_SYS_LED_PIN=$5" >> ${DEFCONFIG}
+	echo "CONFIG_MT7621_SYS_LED_LEVEL=0" >> ${DEFCONFIG}
+else
+	echo "System LED is disabled!"
+	echo "# CONFIG_MT7621_SYS_LED is not set" >> ${DEFCONFIG}
+fi
+
+if [ $6 -ge 400 -a $6 -le 1200 ]; then
+	echo "set CPU frequency: $6 MHz"
+	echo "CONFIG_MT7621_CPU_FREQ_LEGACY=$6" >> ${DEFCONFIG}
+else
+	echo "Invalid CPU Frequency!"
+	exit 1
+fi
+
+echo "set DRAM frequency: $7"
+echo "CONFIG_MT7621_DRAM_FREQ_$7_LEGACY=y" >> ${DEFCONFIG}
+
+echo "Parse DDR2 init parameters: $8"
+case $8 in
+64MiB)
+	echo "CONFIG_MT7621_DRAM_DDR2_512M_LEGACY=y" >> ${DEFCONFIG}
+	;;
+128MiB)
+	echo "CONFIG_MT7621_DRAM_DDR2_1024M_LEGACY=y" >> ${DEFCONFIG}
+	;;
+W9751G6KB-64MiB-1066MHz)
+	echo "CONFIG_MT7621_DRAM_DDR2_512M_W9751G6KB_A02_1066MHZ_LEGACY=y" >> ${DEFCONFIG}
+	;;
+W971GG6KB25-128MiB-800MHz)
+	echo "CONFIG_MT7621_DRAM_DDR2_1024M_W971GG6KB25_800MHZ_LEGACY=y" >> ${DEFCONFIG}
+	;;
+W971GG6KB18-128MiB-1066MHz)
+	echo "CONFIG_MT7621_DRAM_DDR2_1024M_W971GG6KB18_1066MHZ_LEGACY=y" >> ${DEFCONFIG}
+	;;
+esac
+
+echo "Parse DDR3 init parameters: $9"
+case $9 in
+128MiB)
+	echo "CONFIG_MT7621_DRAM_DDR3_1024M_LEGACY=y" >> ${DEFCONFIG}
+	;;
+128MiB-KGD)
+	echo "CONFIG_MT7621_DRAM_DDR3_1024M_KGD_LEGACY=y" >> ${DEFCONFIG}
+	;;
+256MiB)
+	echo "CONFIG_MT7621_DRAM_DDR3_2048M_LEGACY=y" >> ${DEFCONFIG}
+	;;
+512MiB)
+	echo "CONFIG_MT7621_DRAM_DDR3_4096M_LEGACY=y" >> ${DEFCONFIG}
+	;;
+esac
+
+make mt7621_build_defconfig
+make CROSS_COMPILE=${Toolchain} STAGING_DIR=${Staging}
+make savedefconfig
+mkdir archive
+cat defconfig > archive/mt7621_defconfig
+mv u-boot-mt7621.bin archive/
+mv u-boot.img archive/
