7b28ae2fe267cccadde5d4d8ca4e23d7351887ef
Shiji Yang <yangshiji66@qq.com>
2022-10-13 11:22:25 +0800

    CI: some little improvements
    optimization for DDR parameters options
    enable choose baud rate 57600 or 115200
    disable verbose output during DRAMC initialization
    
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

--- customize.sh
+++ customize.sh
@@ -16,8 +16,8 @@ cd $(dirname "$0")
 # $5	number: sysled gpio
 # $6	number: cpu frequency
 # $7	number: ram frequency
-# $8	string: ddr2 param
-# $9	string: ddr3 param
+# $8	string: ddr param
+# $9	string: baud rate
 
 echo "Parse flash type: $1"
 # simple check if partition table is valid
@@ -26,7 +26,7 @@ if [ -z $( echo -n "$2" | grep '),-(firmware)') ]; then
 	exit 1
 fi
 DEFCONFIG="configs/mt7621_build_defconfig"
-if [ "$1" = 'SPI' ]; then
+if [ "$1" = 'NOR' ]; then
 	cp configs/mt7621_nor_template_defconfig ${DEFCONFIG}
 	echo -e "CONFIG_MTDPARTS_DEFAULT=\"mtdparts=raspi:$2\"" >> ${DEFCONFIG}
 elif [ "$1" = 'NAND' ]; then
@@ -39,13 +39,13 @@ fi
 echo "set partition table: $2"
 
 echo "set kernel offset: $3"
-if [ "$1" = 'SPI' ]; then
+if [ "$1" = 'NOR' ]; then
 	echo "CONFIG_DEFAULT_NOR_KERNEL_OFFSET=$3" >> ${DEFCONFIG}
 else
 	echo "CONFIG_DEFAULT_NAND_KERNEL_OFFSET=$3" >> ${DEFCONFIG}
 fi
 
-if [ $4 -ge 0 -a $4 -le 48 ]; then
+if [ "$4" -ge 0 -a "$4" -le 48 ]; then
 	echo "set reset button pin: $4"
 	echo "CONFIG_MT7621_RST_BTN_PIN=$4" >> ${DEFCONFIG}
 	# if reset button is triggered by high level, change '0' to '1' here
@@ -55,7 +55,7 @@ else
 	echo "# CONFIG_MT7621_RST_BTN is not set" >> ${DEFCONFIG}
 fi
 
-if [ $5 -ge 0 -a $5 -le 48 ]; then
+if [ "$5" -ge 0 -a "$5" -le 48 ]; then
 	echo "set system led pin: $5"
 	echo "CONFIG_MT7621_SYS_LED_PIN=$5" >> ${DEFCONFIG}
 	echo "CONFIG_MT7621_SYS_LED_LEVEL=0" >> ${DEFCONFIG}
@@ -64,7 +64,7 @@ else
 	echo "# CONFIG_MT7621_SYS_LED is not set" >> ${DEFCONFIG}
 fi
 
-if [ $6 -ge 400 -a $6 -le 1200 ]; then
+if [ "$6" -ge 400 -a "$6" -le 1200 ]; then
 	echo "set CPU frequency: $6 MHz"
 	echo "CONFIG_MT7621_CPU_FREQ_LEGACY=$6" >> ${DEFCONFIG}
 else
@@ -72,43 +72,46 @@ else
 	exit 1
 fi
 
-echo "set DRAM frequency: $7"
+echo "set DRAM frequency: $7 MHz"
 echo "CONFIG_MT7621_DRAM_FREQ_$7_LEGACY=y" >> ${DEFCONFIG}
 
-echo "Parse DDR2 init parameters: $8"
-case $8 in
-64MiB)
+echo "Parse DDR init parameters: $8"
+case "$8" in
+DDR2-64MiB)
 	echo "CONFIG_MT7621_DRAM_DDR2_512M_LEGACY=y" >> ${DEFCONFIG}
 	;;
-128MiB)
+DDR2-128MiB)
 	echo "CONFIG_MT7621_DRAM_DDR2_1024M_LEGACY=y" >> ${DEFCONFIG}
 	;;
-W9751G6KB-64MiB-1066MHz)
+DDR2-W9751G6KB-64MiB-1066MHz)
 	echo "CONFIG_MT7621_DRAM_DDR2_512M_W9751G6KB_A02_1066MHZ_LEGACY=y" >> ${DEFCONFIG}
 	;;
-W971GG6KB25-128MiB-800MHz)
+DDR2-W971GG6KB25-128MiB-800MHz)
 	echo "CONFIG_MT7621_DRAM_DDR2_1024M_W971GG6KB25_800MHZ_LEGACY=y" >> ${DEFCONFIG}
 	;;
-W971GG6KB18-128MiB-1066MHz)
+DDR2-W971GG6KB18-128MiB-1066MHz)
 	echo "CONFIG_MT7621_DRAM_DDR2_1024M_W971GG6KB18_1066MHZ_LEGACY=y" >> ${DEFCONFIG}
+	;;
+DDR3-128MiB)
+	echo "CONFIG_MT7621_DRAM_DDR3_1024M_LEGACY=y" >> ${DEFCONFIG}
+	;;
+DDR3-256MiB)
+	echo "CONFIG_MT7621_DRAM_DDR3_2048M_LEGACY=y" >> ${DEFCONFIG}
+	;;
+DDR3-512MiB)
+	echo "CONFIG_MT7621_DRAM_DDR3_4096M_LEGACY=y" >> ${DEFCONFIG}
+	;;
+DDR3-128MiB-KGD)
+	echo "CONFIG_MT7621_DRAM_DDR3_1024M_KGD_LEGACY=y" >> ${DEFCONFIG}
 	;;
 esac
 
-echo "Parse DDR3 init parameters: $9"
-case $9 in
-128MiB)
-	echo "CONFIG_MT7621_DRAM_DDR3_1024M_LEGACY=y" >> ${DEFCONFIG}
-	;;
-128MiB-KGD)
-	echo "CONFIG_MT7621_DRAM_DDR3_1024M_KGD_LEGACY=y" >> ${DEFCONFIG}
-	;;
-256MiB)
-	echo "CONFIG_MT7621_DRAM_DDR3_2048M_LEGACY=y" >> ${DEFCONFIG}
-	;;
-512MiB)
-	echo "CONFIG_MT7621_DRAM_DDR3_4096M_LEGACY=y" >> ${DEFCONFIG}
-	;;
-esac
+echo "Set baud rate: $9"
+if [ "$9" = '57600' ]; then
+	echo "CONFIG_BAUDRATE=57600" >> ${DEFCONFIG}
+else
+	echo "CONFIG_BAUDRATE=115200" >> ${DEFCONFIG}
+fi
 
 make mt7621_build_defconfig
 make CROSS_COMPILE=${Toolchain} STAGING_DIR=${Staging}
