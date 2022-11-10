3252b45b0712bb00e6928fa4b125a7a4adb216bf
Simon Glass <sjg@chromium.org>
2022-11-09 19:14:39 -0700

    image: Correct strncpy() warning with image_set_name()
    gcc 12 seems to warn on strncpy() as a matter of course. Rewrite the code
    a different way to do the same thing, to avoid the warning.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/image.h
+++ include/image.h
@@ -796,7 +796,13 @@ image_set_hdr_b(comp)		/* image_set_comp */
 
 static inline void image_set_name(image_header_t *hdr, const char *name)
 {
-	strncpy(image_get_name(hdr), name, IH_NMLEN);
+	/*
+	 * This is equivalent to: strncpy(image_get_name(hdr), name, IH_NMLEN);
+	 *
+	 * Use the tortured code below to avoid a warning with gcc 12. We do not
+	 * want to include a nul terminator if the name is of length IH_NMLEN
+	 */
+	memcpy(image_get_name(hdr), name, strnlen(name, IH_NMLEN));
 }
 
 int image_check_hcrc(const image_header_t *hdr);
